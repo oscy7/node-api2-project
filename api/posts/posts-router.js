a92c7bc83f5f0db64b00ee9b742e9b7193b71abe
Oscar Hernandez <oscarh425@outlook.com>
2022-01-11 16:37:08 -0800

    finished get by id

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -18,8 +18,23 @@ router.get('/', (req, res) => {
             })
         })
 })
-router.get('/:id', (req, res) => {
-    res.json('asdfa')
+router.get('/:id', async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id)
+        if(!post) {
+            res.status(404).json({
+                message:'The post with the specified ID does not exist'
+            })
+        }else{
+            res.json(post)
+        }
+    }catch (err) {
+        res.status(500).json({
+            message:"The posts information could not be retrieved",
+            err:err.message,
+            stack: err.stack
+        })
+    }
 })
 router.post('/', (req, res) => {
 
